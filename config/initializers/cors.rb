3a66f99c8605ff0c501fb4ef6ba167a84e57e7af
Anthony Hansord <tonyhansorddev@gmail.com>
2023-07-29 18:43:20 +1000

    Modified cors settings for deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://booklogue-backend.onrender.com"
+    origins "localhost:3000", "https://booklogue.netlify.app"
 
     resource "*",
              headers: :any,
